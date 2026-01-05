c861870d107fd26ebf8589ba5137c87131043c3b
ATmilkyway <natnaelasegd0507@gmail.com>
2026-01-05 03:56:20 +0300

    Dropdown menus

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AppSideBar from "@/components/AppSideBar";
+import NavBar from "@/components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +27,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex`}
       >
-        {children}
+        <AppSideBar />
+        <main className="w-full">
+          <NavBar />
+          <div className="px-4">{children}</div>
+        </main>
       </body>
     </html>
   );
