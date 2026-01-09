819607625a2e38717e1efc5450928342223e3f1d
ATmilkyway <natnaelasegd0507@gmail.com>
2026-01-09 03:11:09 +0300

    AppBarChart

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import AppBarChart from "@/components/AppBarChart";
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-3'>
-      <div className="bg-primary-foreground p-8 rounded-lg lg:col-span-2 xl:col-end-1 2xl:col-end-2"></div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-3">
+      <div className="bg-primary-foreground p-8 rounded-lg lg:col-span-2 xl:col-end-1 2xl:col-end-2">
+        <AppBarChart />
+      </div>
       <div className="bg-primary-foreground p-8 rounded-lg "></div>
       <div className="bg-primary-foreground p-8 rounded-lg "></div>
       <div className="bg-primary-foreground p-8 rounded-lg "></div>
       <div className="bg-primary-foreground p-8 rounded-lg lg:col-span-2 xl:col-end-1 2xl:col-end-2"></div>
       <div className="bg-primary-foreground p-8 rounded-lg "></div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
